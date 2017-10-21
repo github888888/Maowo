935053c7b29525708b76308247c67a5b909a7826
zhoushaopei <512313620@qq.com>
2017-10-21 22:27:59 +0800

    设置清除缓存

--- app/src/main/java/com/music/maowo/activity/mine/SettingActivity.java
+++ app/src/main/java/com/music/maowo/activity/mine/SettingActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
+import com.music.maowo.MyApplication;
 import com.music.maowo.R;
+import com.music.maowo.Utils.DevicesUtils;
+import com.music.maowo.Utils.GlideCacheUtil;
 import com.music.maowo.activity.BaseActivity;
 import com.music.maowo.anno.Layout;
 import com.music.maowo.view.SwitchButtonNew;
@@ -25,6 +29,10 @@ public class SettingActivity extends BaseActivity implements CompoundButton.OnCh
     SwitchButtonNew mSleepMode;
     @BindView(R.id.shake_change)
     SwitchButtonNew mShakeChange;
+    @BindView(R.id.iv_cache_size)
+    TextView mCacheSize;
+    @BindView(R.id.iv_version)
+    TextView mVersion;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,9 +40,13 @@ public class SettingActivity extends BaseActivity implements CompoundButton.OnCh
 
         mSleepMode.setOnCheckedChangeListener(this);
         mShakeChange.setOnCheckedChangeListener(this);
+        String size = GlideCacheUtil.getInstance().getCacheSize(this);
+        String version = DevicesUtils.getVersionName(this);
+        mCacheSize.setText(size);
+        mVersion.setText(version);
     }
 
-    @OnClick({R.id.iv_back, R.id.iv_feedback, R.id.iv_upgrade})
+    @OnClick({R.id.iv_back, R.id.iv_feedback, R.id.iv_upgrade, R.id.iv_clear_cache})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_back:
@@ -42,6 +54,10 @@ public class SettingActivity extends BaseActivity implements CompoundButton.OnCh
                 break;
             case R.id.iv_feedback:
                 FeedbackActivity.actionInstance(this);
+                break;
+            case R.id.iv_clear_cache:
+                GlideCacheUtil.getInstance().clearImageAllCache(this);
+                MyApplication.toast(this, "缓存清理中....请稍候");
                 break;
             case R.id.iv_upgrade:
 
